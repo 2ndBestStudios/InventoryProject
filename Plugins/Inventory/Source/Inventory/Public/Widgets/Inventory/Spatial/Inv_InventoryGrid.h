d25f26e40440d8c4fc3f8602f573dbedf6452f0b
Christopher Moroshan <2ndbeststudios@gmail.com>
2025-11-18 14:39:15 +0900

    Filling in Stacks

--- Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Spatial/Inv_InventoryGrid.h
+++ Plugins/Inventory/Source/Inventory/Public/Widgets/Inventory/Spatial/Inv_InventoryGrid.h
@@ -192,6 +192,10 @@ private:
 	bool ShouldConsumeHoverItemStacks(const int32 HoveredStackCount, const int32 RoomInClickedSlot) const;
 
 	void ConsumeHoverItemStacks(const int32 ClickedStackCount, const int32 HoveredStackCount, const int32 Index);
+
+	bool ShouldFillInStack(const int32 RoomInClickedSlot, const int32 HoveredStackCount) const;
+
+	void FillInStack(const int32 FillAmount, const int32 Remainder, const int32 Index);
 	
 	// Properties for Visible and Hidden Mouse Cursor 
 	UUserWidget* GetVisibleCursorWidget();
